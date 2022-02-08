31bbbd2ea45e243cc5814cd3953a9fc6d28961e8
Lawrence <lawrencesalinas@yahoo.com>
2022-02-08 06:06:48 -0500

    django poractuce fisnisehd dynamic urls

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,4 @@
+from urllib import response
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
@@ -23,15 +24,10 @@ def index(request):
     
     for month in months:
         capitalized_month = month.capitalize()
-        list_items += f"<li><a href="">{capitalized_month}</a></li>"
-    
-    
-    response_data = """
-    <ul>
-    
-    
-    """
-    return HttpResponse('s')
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
 
 def monthly_challenge_by_number(request,month):
     months = list(monthly_challenges.keys())
@@ -39,7 +35,7 @@ def monthly_challenge_by_number(request,month):
         return HttpResponseNotFound('invalid month')
     redirect_month = months[month - 1]
     redirect_path = reverse("month-challenge", args=[redirect_month])
-    return HttpResponseRedirect(redirect_path)
+    return HttpResponseRedirect(redirect_path) 
 
 def monthly_challenge(request, month):
     try:
